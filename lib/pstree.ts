805362dd27d7f1adbbd573d2d507552e708c3f2b
BreadLoeuf <breadloeuf@gmail.com>
2025-11-18 02:15:18 -0600

    added pstree.ts

---
+++ lib/pstree.ts
@@ -0,0 +1,23 @@
+type TreeNode = {
+    type: 'leaf',
+    name: string,
+} | {
+    type: 'node',
+    name: string,
+    children: TreeNode[],
+};
+
+const aNode: TreeNode = {
+    type: 'leaf',
+    name: 'a',
+};
+
+const bNode: TreeNode = {
+    type: 'node',
+    name: 'b', 
+    children: [aNode],
+};
+
+const input = '[S [NP [Pron This] [N is]] [VP [V a] [N test]]]';
+const splitInp = input.split(/([ \[\]])/).filter(c => c && c !== ' ');
+console.log(splitInp);
